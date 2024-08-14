1b91a9e98b75769d68c420d334f543eea080e063
Daniel <daniel@trakr.it>
2024-08-14 13:06:31 +0800

    commit

--- packages/medusa-strapi/config/database.js
+++ packages/medusa-strapi/config/database.js
@@ -116,12 +116,16 @@ function DatabaseConfiguration({ env }) {
 			client: 'postgres',
 			connection: {
 				host: env('DATABASE_HOST', '127.0.0.1'),
-				port: env.int('DATABASE_PORT', '5432'),
-				database: env('DATABASE_NAME', 'postgres_strapi'),
-				user: env('DATABASE_USERNAME', 'postgres'),
-				password: env('DATABASE_PASSWORD', 'postgres'),
+				port: env.int('DATABASE_PORT', 5432),
+				database: env('DATABASE_NAME', 'strapidb'),
+				user: env('DATABASE_USERNAME', ''),
+				password: env('DATABASE_PASSWORD', ''),
+        ssl: env.bool("DATABASE_SSL", false) && {
+            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+        },
 				schema,
 			},
+      debug: false,
 		},
 	};
 
