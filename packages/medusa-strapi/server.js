7211dbad6c9b98f0052c2bf6772861f32945c451
DANIEL CAMPBELL <daniel@trakr.it>
2024-08-30 20:04:56 +0100

    commit

--- packages/medusa-strapi/server.js
+++ packages/medusa-strapi/server.js
@@ -1,2 +1,13 @@
 const strapi = require('@strapi/strapi');
 strapi(/* {...} */).start();
+
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", "https://xyz.com"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "default-secret-here"),
+    },
+  },
+});
