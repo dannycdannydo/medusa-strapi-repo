a664fd290708e65526d8e5455152d7abf5ab8948
DANIEL CAMPBELL <daniel@trakr.it>
2024-09-08 21:10:30 +0100

    commit

---
+++ packages/medusa-strapi/src/api/general-image/controllers/general-image.ts
@@ -0,0 +1,7 @@
+/**
+ * general-image controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::general-image.general-image');
---
+++ packages/medusa-strapi/src/api/general-image/services/general-image.ts
@@ -0,0 +1,7 @@
+/**
+ * general-image service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::general-image.general-image');
